f444f18c20b0d574a952262b41c98fafcb463217
ivieomofoma <ivie.omofoma@gmail.com>
2023-08-23 11:58:42 +0100

    react jsx component

--- src/Components/Productcard.jsx
+++ src/Components/Productcard.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import wed3 from './Images/wed3.jpg';
-import './productcard.css'
 
 const Productcard = ({price, name, description, image}) => {
 
   return (
     <div>
-        <Card className='cards' style={{ width: '18rem' }}>
+        <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src={image} />
       <Card.Body>
         <Card.Title>NAME: {name}</Card.Title>
